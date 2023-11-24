9507840ca524f4b45320c8eea0819c892a2ea617
Get15Gonzo <yourgithub@email.com>
2023-11-24 12:23:24 -0700

    Working on the CSS for main

--- capstone-final/src/components/Main.js
+++ capstone-final/src/components/Main.js
@@ -1,15 +1,20 @@
-
+import restaurantFood from '../images/restaurant-food.jpg'
 
 const Main = () => {
     return (
-        <fain>
-            <h1>Little Lemon</h1>
-            <h3>Chicago</h3>
-            <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist</p>
-            <button>
-                Reserve a table
-            </button>
-        </fain>
+        <main>
+            <section className="main--one">
+                <h1 className="main--title">Little Lemon</h1>
+                <h3 className="main--subtitle">Chicago</h3>
+                <p className="main--text"> We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist</p>
+                <button className="main--btn">
+                    Reserve a Table
+                </button>
+            </section>
+            <section className="main--two">
+                    <img src={restaurantFood} alt="food" height="300px" width="200px" />
+            </section>
+        </main>
     )
 }
 
