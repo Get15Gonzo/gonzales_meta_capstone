8bb7655431ccfb6da79b74a7a1a8011446b779ab
Get15Gonzo <yourgithub@email.com>
2023-11-24 17:43:55 -0700

    finished home page

--- capstone-final/src/components/Footer.js
+++ capstone-final/src/components/Footer.js
@@ -1,4 +1,7 @@
-
+import bruschetta from '../images/bruchetta.svg'
+import greekSalad from '../images/greek-salad.jpg'
+import lemonDessert from '../images/lemon-dessert.jpg'
+import bicycle from '../images/bicycle-vector.png'
 
 const Footer = () => {
     return (
@@ -8,26 +11,41 @@ const Footer = () => {
                 <button className="btn">Online Menu</button>
             </heading>
             <cards>
-                <section>
-                    <p>Greek Salad</p>
-                    <p>12.99$</p>
+                <section className="footer--card">
+                    <img className="card--img" src={greekSalad} alt="Greek salad" />
+                    <cardtitle>
+                        <p>Greek Salad</p>
+                        <p className="card--price">12.99$</p>
+                    </cardtitle>
                     <p>The famous greek salad of crispy lettuce, peppers, olives, and our Chicago style feta cheese, garnished with crunch garlic and rosemary croutons.</p>
-                    <p>Order A delivery</p>
-                    <img src="bicycle-vector.png" alt="bicycle delivery icon" />
+                    <cardfooter>
+                        <p id="card--delivery">Order A delivery</p>
+                        <img src={bicycle} alt="bicycle delivery icon" width="25px" height="25px" />
+                    </cardfooter>
                 </section>
-                <section>
-                    <p>Bruschetta</p>
-                    <p>12.99$</p>
-                    <p>Our bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.</p>
-                    <p>Order A delivery</p>
-                    <img src="bicycle-vector.png" alt="bicycle delivery icon" />
+                <section  className="footer--card">
+                    <img className="card--img" src={bruschetta} alt="bruschetta is bread with different toppings on it" />
+                    <cardtitle>
+                        <p>Bruschetta</p>
+                        <p className="card--price">12.99$</p>
+                    </cardtitle>
+                    <p>Our bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil. <br/> <br/> <br/> </p>
+                    <cardfooter>
+                        <p id="card--delivery">Order A delivery</p>
+                        <img src={bicycle} alt="bicycle delivery icon" width="25px" height="25px" />
+                    </cardfooter>
                 </section>
-                <section>
-                    <p>Lemon Dessert</p>
-                    <p>12.99$</p>
-                    <p>This comes straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined</p>
-                    <p>Order A delivery</p>
-                    <img src="bicycle-vector.png" alt="bicycle delivery icon" />
+                <section  className="footer--card">
+                    <img className="card--img" src={lemonDessert} alt="lemon dessert" />
+                    <cardtitle>
+                        <p>Lemon Dessert</p>
+                        <p className="card--price">12.99$</p>
+                    </cardtitle>
+                    <p>This comes straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined <br/> <br/> </p>
+                    <cardfooter>
+                        <p id="card--delivery">Order A delivery</p>
+                        <img src={bicycle} alt="bicycle delivery icon" width="25px" height="25px" />
+                    </cardfooter>
                 </section>
             </cards>
         </footer>
